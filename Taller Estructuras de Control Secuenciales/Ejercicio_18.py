a49b18006ef1c9a9c0666513eba6afb7cbd57851
Julian Ramirez Robayo <jramire23664@universidadean.edu.co>
2021-08-15 09:18:28 -0500

    Ejercicio_18.py
    Ejercicio_18.py

--- Taller Estructuras de Control Secuenciales/Ejercicio_18.py
+++ Taller Estructuras de Control Secuenciales/Ejercicio_18.py
@@ -6,4 +6,4 @@ pnt2 = (pb-(pbtu*adt))
 ptb = (pnt2/pb)*100
 irs = (pb*ad*4)/100
 print("El porncetaje de pagos anules es igaules a: ""{:.0F}".format(ptb),("%"))
-print("El monto de intereses pagados anualmente es de: ""{:.0F}".format(irs),("Bs"))
+print("El monto de intereses pagados anualmente es de: ""{:.0F}".format(irs),("BS"))
