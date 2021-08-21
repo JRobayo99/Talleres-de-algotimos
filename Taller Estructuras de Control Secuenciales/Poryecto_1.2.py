3a458b67e277182fb456950050acb7af96640d43
Julian Ramirez Robayo <jramire23664@universidadean.edu.co>
2021-08-21 09:37:08 -0500

    Create Poryecto_1.2.py

---
+++ Taller Estructuras de Control Secuenciales/Poryecto_1.2.py
@@ -0,0 +1,12 @@
+print ("__Menu del dia__")
+print("1) desayuno")
+print("2) alumerzo")
+print("3) Cena")
+
+tipom= int(input ("Selecciona el tipo de menu:  "))
+if tipom==1:
+   print("desayuno")
+elif tipom==2:
+    print("Desay")
+elif tipom==3:
+    print("Pizza")
