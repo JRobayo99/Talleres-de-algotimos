a137e443569485554b11438f863f00a78f69f736
Julian Ramirez Robayo <jramire23664@universidadean.edu.co>
2021-08-28 11:41:02 -0500

    Ejercicio_1.py
    Ejercicio_1.py

---
+++ Taller de Estructuras de Control Repetición/Ejercicio_1.py
@@ -0,0 +1,16 @@
+k =int(input("Ingrese la variable K: "))
+n=int(input("Ingrese la variable N: "))
+p=1    
+while (k>=0 and n>=0 and p>=0) :
+  
+  if(k<n):
+        n1= n-p
+        print(n1)
+        p=p+1
+  if(k==n1):
+    break
+            
+ 
+  
+           
+ 
