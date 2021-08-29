48d715170585b05f3717b76e00ab035326506588
Julian Ramirez Robayo <jramire23664@universidadean.edu.co>
2021-08-28 19:40:12 -0500

    Ejercicio_6.py
    Ejercicio_6.py

--- Taller de Estructuras de Control Repetición/Ejercicio_6.py
+++ Taller de Estructuras de Control Repetición/Ejercicio_6.py
@@ -1,9 +1,10 @@
-cot=0
-res=0
 num=int(input("Ingrese númerador: "))
 denom=int(input("Ingrese denominador: "))
+
 while(num>= denom):
     num=num - denom
     res = num
     print("Los resultares osn igual a: "+str(num))
+cot=0
+res=0
 
