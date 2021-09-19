684ad3a4a21ada2f8347a6bae6eaa845d5abb2dc
Julian Ramirez Robayo <jramire23664@universidadean.edu.co>
2021-09-19 16:14:41 -0500

    nombre.py

---
+++ Taller  nombre tourtle/nombre.py
@@ -0,0 +1,218 @@
+import turtle 
+
+
+tortuga=turtle.Turtle()
+tortuga.pensize(8)
+tortuga.color("blue")
+tortuga.shape('turtle')
+tortuga.left(0)
+tortuga.forward(50)
+tortuga.left(0)
+tortuga.forward(50)
+tortuga.right(180)
+tortuga.forward(50)
+tortuga.left(90)
+tortuga.forward(90)
+tortuga.right(90)
+tortuga.forward(50)
+
+
+tortuga=turtle.Turtle()
+tortuga.shape('turtle')
+tortuga.pensize(8)
+tortuga.color("orange")
+tortuga.penup()
+tortuga.goto((110,0))
+tortuga.pendown()
+tortuga.right(90)
+tortuga.forward(90)
+tortuga.right(270)
+tortuga.forward(70)
+tortuga.left(90)
+tortuga.forward(90)
+
+
+tortuga=turtle.Turtle()
+tortuga.shape('turtle')
+tortuga.pensize(8)
+tortuga.color("green")
+tortuga.penup()
+tortuga.goto((200, 0))
+tortuga.pendown()
+tortuga.right(90)
+tortuga.forward(90)
+tortuga.left(90)
+tortuga.forward(70)
+
+
+tortuga=turtle.Turtle()
+tortuga.shape('turtle')
+tortuga.pensize(8)
+tortuga.color("red")
+tortuga.penup()
+tortuga.goto((310, 0))
+tortuga.pendown()
+tortuga.left(0)
+tortuga.forward(100)
+tortuga.left(180)
+tortuga.forward(50)
+tortuga.left(90)
+tortuga.forward(90)
+tortuga.left(90)
+tortuga.forward(50)
+tortuga.right(180)
+tortuga.forward(100)
+
+
+tortuga=turtle.Turtle()
+tortuga.shape('turtle')
+tortuga.pensize(8)
+tortuga.color("yellow")
+tortuga.penup()
+tortuga.goto((430, -90))
+tortuga.pendown()
+tortuga.left(75)
+tortuga.forward(90)
+tortuga.right(150)
+tortuga.forward(90)
+tortuga.left(180)
+tortuga.forward(30)
+tortuga.left(75)
+tortuga.forward(25)
+
+tortuga=turtle.Turtle()
+tortuga.shape('turtle')
+tortuga.pensize(8)
+tortuga.color("purple")
+tortuga.penup()
+tortuga.goto((500, -90))
+tortuga.pendown()
+tortuga.left(90)
+tortuga.forward(90)
+tortuga.right(135)
+tortuga.forward(115)
+tortuga.left(135)
+tortuga.forward(90)
+
+
+
+
+import turtle
+t=turtle.Turtle()
+t.speed(0)
+t.goto((200, 120))
+t.pendown()
+t.pencolor("navy")
+t.pensize(5)
+for x in range (9):
+    t.rt(10)
+    for y in range (5):
+        t.fd(20)
+        t.rt(20)
+    t.rt(50)
+    for z in range (10):
+        t.fd(55)
+        t.rt(55)
+    t.rt(50)
+    
+    
+
+
+t=turtle.Turtle()
+t.speed(0)
+t.penup()
+t.goto((335, -200))
+t.pendown()
+t.pencolor("dark orange")
+t.pensize(5)
+
+
+
+for x in range (10):
+    t.rt(20)
+    for y in range (6):
+        t.fd(19)
+        t.rt(19)
+    t.rt(49)
+    for z in range (11):
+        t.fd(65)
+        t.rt(65)
+    t.rt(49)
+
+import turtle
+t=turtle.Turtle()
+t.speed(0)
+t.goto((400, 120))
+t.pendown()
+t.pencolor("brown")
+t.pensize(5)
+for x in range (9):
+    t.rt(10)
+    for y in range (5):
+        t.fd(20)
+        t.rt(20)
+    t.rt(50)
+    for z in range (10):
+        t.fd(55)
+        t.rt(55)
+    t.rt(50)
+    
+t=turtle.Turtle()
+t.speed(0)
+t.penup()
+t.goto((225, -200))
+t.pendown()
+t.pencolor("aquamarine")
+t.pensize(5)
+
+
+
+for x in range (10):
+    t.rt(20)
+    for y in range (6):
+        t.fd(19)
+        t.rt(19)
+    t.rt(49)
+    for z in range (11):
+        t.fd(65)
+        t.rt(65)
+    t.rt(49)    
+
+import turtle
+t=turtle.Turtle()
+t.speed(0)
+t.goto((575, 120))
+t.pendown()
+t.pencolor("grey")
+t.pensize(5)
+for x in range (9):
+    t.rt(10)
+    for y in range (5):
+        t.fd(20)
+        t.rt(20)
+    t.rt(50)
+    for z in range (10):
+        t.fd(55)
+        t.rt(55)
+    t.rt(50)
+
+t=turtle.Turtle()
+t.speed(0)
+t.penup()
+t.goto((450, -200))
+t.pendown()
+t.pencolor("dark green")
+t.pensize(5)
+
+
+
+for x in range (10):
+    t.rt(20)
+    for y in range (6):
+        t.fd(19)
+        t.rt(19)
+    t.rt(49)
+    for z in range (11):
+        t.fd(65)
+        t.rt(65)
+    t.rt(49)    
